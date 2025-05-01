64e2e7683bf8b83c1b8463cdebee402c2aa13ae0
meSanika07 <sanikakundekar007@gmail.com>
2025-05-01 21:07:09 +0530

    Make deployment scripts executable

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 cd /home/ec2-user/node-app
-node app.js > app.log 2>&1 &
+
+# Stop previous app (if running)
+pkill node
+
+# Start the app in the background
+nohup node app.js > app.log 2>&1 &
